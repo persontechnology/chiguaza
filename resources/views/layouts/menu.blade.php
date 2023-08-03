d78db176715b777d2aef1ae13fbe8d494ae3bc27
persontechnology <persontechnologys@gmail.com>
2023-08-03 11:04:49 -0500

    slider

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,71 @@
+<div class="main-menu__bottom-inner">
+    <div class="main-menu__main-menu-box">
+        <a href="#" class="mobile-nav__toggler"><i class="fa fa-bars"></i></a>
+        <ul class="main-menu__list">
+            <li>
+                <a href="index.html">Inicio</a>
+            </li>
+            <li class="dropdown">
+                <a href="#">Municipal</a>
+                <ul class="sub-menu">
+                    <li><a href="about.html">Misión y Visión</a></li>
+                    <li><a href="our-history.html">Chiguaza</a></li>
+                    <li><a href="team.html">Presidente</a></li>
+                    <li><a href="team-details.html">Team details</a></li>
+                    <li><a href="portfolio.html">Portfolio</a></li>
+                    <li><a href="portfolio-details.html">Portfolio details</a></li>
+                    <li><a href="events.html">Events</a></li>
+                    <li><a href="event-details.html">Event details</a></li>
+                    <li><a href="pricing.html">Pricing</a></li>
+                    <li><a href="faq.html">FAQs</a></li>
+                    <li><a href="404.html">404 error</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#">Tranparencia</a>
+                <ul class="sub-menu">
+                    <li><a href="our-services.html">Services</a></li>
+                    <li><a href="building-permission.html">Building permission</a></li>
+                    <li><a href="driving-license.html">Driving license</a></li>
+                    <li><a href="report-polution.html">Report polution</a></li>
+                    <li><a href="parking-permission.html">Parking permission</a></li>
+                    <li><a href="tax-return.html">Tax return</a></li>
+                    <li><a href="birth-certificate.html">Birth certificate</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#">Departments</a>
+                <ul class="sub-menu">
+                    <li><a href="departments.html">Departments</a></li>
+                    <li><a href="department-details.html">Department details</a></li>
+                </ul>
+            </li>
+
+            <li class="dropdown">
+                <a href="#">News</a>
+                <ul class="sub-menu">
+                    <li><a href="news.html">News</a></li>
+                    <li><a href="news-sidebar.html">News sidebar</a></li>
+                    <li><a href="news-details.html">News details</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#">Shop</a>
+                <ul class="sub-menu">
+                    <li><a href="shop.html">Shop</a></li>
+                    <li><a href="shop-details.html">Shop details</a></li>
+                    <li><a href="cart.html">Cart</a></li>
+                    <li><a href="checkout.html">Checkout</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="contact.html">Contact</a>
+            </li>
+        </ul>
+    </div>
+    <div class="main-menu__right">
+        <div class="main-menu__btn-box">
+            <a href="contact.html" class="thm-btn main-menu__btn">Report Issue</a>
+        </div>
+    </div>
+</div>
---
+++ resources/views/sections/slider.blade.php
@@ -0,0 +1,143 @@
+<!-- Main Sllider Start -->
+<section class="main-slider">
+    <div class="main-slider__carousel owl-carousel owl-theme thm-owl__carousel"
+        data-owl-options='{"loop": true, "items": 1, "navText": ["<span class=\"icon-left-arrow\"></span>","<span class=\"icon-right-arrow\"></span>"], "margin": 0, "dots": true, "nav": true, "animateOut": "slideOutDown", "animateIn": "fadeIn", "active": true, "smartSpeed": 1000, "autoplay": true, "autoplayTimeout": 7000, "autoplayHoverPause": false}'>
+
+        <div class="item main-slider__slide-1">
+            <div class="main-slider__bg"
+                style="background-image: url(assets/images/backgrounds/slider-1-1.jpg);">
+            </div><!-- /.slider-one__bg -->
+            <div class="main-slider__shape-1">
+                <img src="{{ asset('assets/images/shapes/main-slider-shape-1.png') }}" alt="">
+            </div>
+            <div class="main-slider__shape-2">
+                <img src="{{ asset('assets/images/shapes/main-slider-shape-2.png') }}" alt="">
+            </div>
+            <div class="main-slider__shape-3">
+                <img src="{{ asset('assets/images/shapes/main-slider-shape-3.png') }}" alt="">
+            </div>
+            <div class="main-slider__meta-box">
+                <ul class="main-slider__meta list-unstyled">
+                    <li>
+                        <div class="icon">
+                            <span class="fas fa-sun"></span>
+                        </div>
+                        <div class="text">
+                            <p>{{ date('d/m/Y') }}</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="icon">
+                            <span class="fas fa-clock"></span>
+                        </div>
+                        <div class="text">
+                            <p>HORA: 09:28 pm</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="container">
+                <div class="main-slider__content">
+                    <p class="main-slider__sub-title">2 Millions Visitors Every Year</p>
+                    <h2 class="main-slider__title">Safest City in
+                        <br> the World.</h2>
+                    <div class="main-slider__btn-box">
+                        <a href="about.html" class="main-slider__btn thm-btn">Descubrir más</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="item main-slider__slide-2">
+            <div class="main-slider__bg"
+                style="background-image: url(assets/images/backgrounds/slider-1-2.jpg);">
+            </div><!-- /.slider-one__bg -->
+            <div class="main-slider__shape-1">
+                <img src="assets/images/shapes/main-slider-shape-1.png" alt="">
+            </div>
+            <div class="main-slider__shape-2">
+                <img src="assets/images/shapes/main-slider-shape-2.png" alt="">
+            </div>
+            <div class="main-slider__shape-3">
+                <img src="assets/images/shapes/main-slider-shape-3.png" alt="">
+            </div>
+            <div class="main-slider__meta-box">
+                <ul class="main-slider__meta list-unstyled">
+                    <li>
+                        <div class="icon">
+                            <span class="fas fa-sun"></span>
+                        </div>
+                        <div class="text">
+                            <p>Today: 32 0C</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="icon">
+                            <span class="fas fa-clock"></span>
+                        </div>
+                        <div class="text">
+                            <p>Time: 09:28 pm</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="container">
+                <div class="main-slider__content">
+                    <p class="main-slider__sub-title">2 Millions Visitors Every Year</p>
+                    <h2 class="main-slider__title">Safest City in
+                        <br> the World.</h2>
+                    <div class="main-slider__btn-box">
+                        <a href="about.html" class="main-slider__btn thm-btn">Discover More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="item main-slider__slide-3">
+            <div class="main-slider__bg"
+                style="background-image: url(assets/images/backgrounds/slider-1-3.jpg);">
+            </div><!-- /.slider-one__bg -->
+            <div class="main-slider__shape-1">
+                <img src="assets/images/shapes/main-slider-shape-1.png" alt="">
+            </div>
+            <div class="main-slider__shape-2">
+                <img src="assets/images/shapes/main-slider-shape-2.png" alt="">
+            </div>
+            <div class="main-slider__shape-3">
+                <img src="assets/images/shapes/main-slider-shape-3.png" alt="">
+            </div>
+            <div class="main-slider__meta-box">
+                <ul class="main-slider__meta list-unstyled">
+                    <li>
+                        <div class="icon">
+                            <span class="fas fa-sun"></span>
+                        </div>
+                        <div class="text">
+                            <p>Today: 32 0C</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="icon">
+                            <span class="fas fa-clock"></span>
+                        </div>
+                        <div class="text">
+                            <p>Time: 09:28 pm</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="container">
+                <div class="main-slider__content">
+                    <p class="main-slider__sub-title">2 Millions Visitors Every Year</p>
+                    <h2 class="main-slider__title">Safest City in
+                        <br> the World.</h2>
+                    <div class="main-slider__btn-box">
+                        <a href="about.html" class="main-slider__btn thm-btn">Discover More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+<!-- Main Sllider Start -->
